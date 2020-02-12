a0315fd547497c71257bd08887492c06f677b7f0
mlad9304 <mlad9304@gmail.com>
2020-02-12 16:47:52 +0300

    Added default syncfusion grid

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -6,13 +6,61 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+} from '@syncfusion/ej2-react-grids';
+import { orderDetails } from './data';
 
 export default function HomePage() {
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+    <div className="material">
+      <div className="control-pane">
+        <div className="control-section">
+          <GridComponent dataSource={orderDetails} height="100%">
+            <ColumnsDirective>
+              <ColumnDirective
+                field="OrderID"
+                headerText="Order ID"
+                width="120"
+                textAlign="Right"
+              />
+              <ColumnDirective
+                field="CustomerName"
+                headerText="Customer Name"
+                width="150"
+              />
+              <ColumnDirective
+                field="OrderDate"
+                headerText="Order Date"
+                width="130"
+                format="yMd"
+                textAlign="Right"
+              />
+              <ColumnDirective
+                field="Freight"
+                headerText="Freight"
+                width="120"
+                format="C2"
+                textAlign="Right"
+              />
+              <ColumnDirective
+                field="ShippedDate"
+                headerText="Shipped Date"
+                width="130"
+                format="yMd"
+                textAlign="Right"
+              />
+              <ColumnDirective
+                field="ShipCountry"
+                headerText="Ship Country"
+                width="150"
+              />
+            </ColumnsDirective>
+          </GridComponent>
+        </div>
+      </div>
+    </div>
   );
 }
